89d80502856caec479fdda0d9e183be0418f1adc
Jay R. Wren <jrwren@xmtp.net>
2019-10-21 20:48:04 -0400

    add args to config

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -11,7 +11,10 @@ func encode(s string, d chan bool) {
 	fn := filepath.Base(s)
 	oname := fn[:strings.LastIndex(fn, ".")] + config.Format
 	log.Printf("Encoding Start: %s", s)
-	cmd := exec.Command("HandBrakeCLI", "-i", s, "-o", config.OutputDir+oname, "-Z", config.Preset, "--decomb=bob")
+	cmdargs := []string{"-i", s, "-o", config.OutputDir + oname,
+		"-Z", config.Preset, "--decomb=bob"}
+	cmdargs = append(cmdargs, config.Args...)
+	cmd := exec.Command("HandBrakeCLI", cmdargs...)
 	if err := cmd.Start(); err != nil {
 		log.Println("Error converting video")
 	}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module merlin
+
+go 1.13
+
+require github.com/rjeczalik/notify v0.9.2
